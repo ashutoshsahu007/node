c511e6874f2b91e650c519c874b9f254d114a297
ashutosh sahu <ppaskumar@gmail.com>
2025-06-19 11:19:15 +0530

    add all for live on render

--- db.js
+++ db.js
@@ -3,8 +3,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // Define the MongoDB connection URL
-// const mongoURL = process.env.MONGO_URL || "mongodb://localhost:27017/aaa";
-const mongoURL = process.env.MONGO_URL;
+// const mongoURL = process.env.MONGO_URL_LOCAL || "mongodb://localhost:27017/aaa";
+const mongoURL = process.env.MONGO_URL || process.env.MONGO_URL_LOCAL;
 
 // Connect to MongoDB using Mongoose
 mongoose.connect(mongoURL);
