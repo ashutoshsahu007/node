bcef424e3c0b6a30ed97e7ef4bc192e9e7d36adc
ashutosh sahu <ppaskumar@gmail.com>
2025-06-19 11:44:11 +0530

    add all

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use("/person", personRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Kya abhishek jii ");
 });
 
 app.listen(PORT, () => {
